f10c75bc2a57ba677cc97694ecfb1ec97dc38f84
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-04-10 21:25:14 +1200

    my first Flutter app completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,24 +14,136 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.white,
-                child: Text('Container 1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Profile_photo.jpg'),
               ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
+              Text(
+                'Harry Kang',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                ),
               ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.red,
-                child: Text('Container 3'),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 20.0,
+                    color: Colors.teal[100],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width:150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                  // color: Colors.deepOrange,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0,
+                horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '64+000 000 0000',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro'
+                    ),
+                  ),
+
+                ),
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0,
+                    horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'gnsqud24@naver.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0,
+                    horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.school,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'Masters of Software Engineering',
+                    style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0,
+                horizontal: 25.0),
+                child: Column(
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(Icons.pending_actions_sharp),
+                      title: Text(
+                          'Skill Sets'
+                      ),
+                      subtitle: Text(
+                          'Flutter, Android, Python'
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        TextButton(
+                          child: const Text('HIRE'),
+                          onPressed: () {
+                            /* ... */
+                          },
+                        ),
+                        const SizedBox(width: 8),
+                        TextButton(
+                          child: const Text(
+                              'LISTEN'
+                          ),
+
+                          onPressed: () {
+                            /* ... */
+                          },
+                        ),
+                        const SizedBox(width: 8),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
